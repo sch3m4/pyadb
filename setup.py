92e7e5368ca51c81b6648ba7bb5af4f3d408fb22
Chema García <chema@safetybits.net>
2015-02-16 18:05:35 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(fname):
 
 setup(
     name = "pyadb",
-    version = "0.1.1",
+    version = "0.1.2",
     author = "Chema Garcia",
     author_email = "chema@safetybits.net",
     description = ("Simple python module to interact with the ADB tool"),
