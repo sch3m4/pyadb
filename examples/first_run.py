b8e1ede29a94e4e456477c33a9fc2d95f815000c
Chema García <chema@safetybits.net>
2018-08-07 19:53:16 +0200

    python 3 compatible

--- examples/first_run.py
+++ examples/first_run.py
@@ -6,20 +6,24 @@
 try:
     import sys
     from pyadb import ADB
-except ImportError,e:
+except ImportError as e:
     # should never be reached
-    print "[f] Required module missing. %s" % e.args[0]
+    print ("[f] Required module missing. %s" % e.args[0])
     sys.exit(-1)
 
 def main():
     # creates the ADB object
     adb = ADB()
     # IMPORTANT: You should supply the absolute path to ADB binary 
-    if adb.set_adb_path('/home/chema/.android-sdks/platform-tools/adb') is True:
-        print "Version: %s" % adb.get_version()
+    if adb.set_adb_path('/usr/bin/adb') is True:
+        print ("Version: %s" % adb.get_version())
     else:
-        print "Check ADB binary path"
+        print ("Check ADB binary path")
+
+    apps = adb.shell_command("pm list packages")
+    for app in apps:
+        path = adb.shell_command("pm path {}".format(app.split(':')[1]))
+        print ("{}: {}".format(app,path))
 
 if __name__ == "__main__":
     main()
-    
