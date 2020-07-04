d276e4006c00ee37f1eda1ddb9be8c3436d33406
Julian-O <kraken@somethinkodd.com>
2020-07-04 13:03:26 +1000

    Handle Windows paths. Prettied up output.

--- examples/first_run.py
+++ examples/first_run.py
@@ -3,8 +3,8 @@
 # Very basic PyADB example
 #
 
+import sys
 try:
-    import sys
     from pyadb import ADB
 except ImportError as e:
     # should never be reached
@@ -15,11 +15,11 @@ except ImportError as e:
 def main():
     # creates the ADB object
     adb = ADB()
-    # IMPORTANT: You should supply the absolute path to ADB binary 
-    if adb.set_adb_path('/usr/bin/adb') is True:
-        print(("Version: %s" % adb.get_version()))
-    else:
-        print("Check ADB binary path")
+    # set ADB path, using a couple of popular addresses.
+    try:
+        adb.set_adb_path('~/android-sdk-linux/platform-tools/adb')
+    except FileNotFoundError:
+        adb.set_adb_path(r'C:\Android\android-sdk\platform-tools\adb.exe')
 
     apps = adb.shell_command("pm list packages")
     for app in apps:
